53572267e119d3355143b4c060729e3d52f95961
Luke <ljm3103@rit.edu>
2022-10-18 13:21:25 -0400

    Adding count error handling

--- cas_lookup.py
+++ cas_lookup.py
@@ -13,12 +13,12 @@ headers = {'accept': 'application/json'}
 def cas_lookup(query_string):
     #curl -X GET "https://commonchemistry.cas.org/api/search?q=dmap" -H  "accept: application/json"
     res = requests.get("https://commonchemistry.cas.org/api/search?q={}".format(query_string), headers=headers)
-    if res.status_code == 200:
+    if res.status_code == 200 and res.json().get('count') > 0:
         # grab first result and the "rn" attribute inside of it, that's the CAS number for the first result
         cas_number = res.json()['results'][0]['rn']
         return cas_number
 
-    return None
+    return "Not Found"
 
 def main():
     # loop through every line of STDIN
