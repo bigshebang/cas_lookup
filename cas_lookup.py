ff90905e5e843f0f4a76049538c05887003dfef5
Luke <ljm3103@rit.edu>
2022-10-18 13:12:05 -0400

    Add some comments for readability

--- cas_lookup.py
+++ cas_lookup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 '''
+    A script that takes in a list of chemical names from STDIN and looks them up on cas.org one by
+    one and prints the corresponding CAS number.
 '''
+
 import sys
 import fileinput
 import requests
@@ -11,15 +14,16 @@ def cas_lookup(query_string):
     #curl -X GET "https://commonchemistry.cas.org/api/search?q=dmap" -H  "accept: application/json"
     res = requests.get("https://commonchemistry.cas.org/api/search?q={}".format(query_string), headers=headers)
     if res.status_code == 200:
-        # grab first result and the "rn" attribute inside of it
+        # grab first result and the "rn" attribute inside of it, that's the CAS number for the first result
         cas_number = res.json()['results'][0]['rn']
         return cas_number
 
     return None
 
 def main():
+    # loop through every line of STDIN
     for line in fileinput.input():
-        # strip newlines and make sure there's a slash in there
+        # strip newlines
         stripped_line = line.rstrip()
         print(cas_lookup(stripped_line))
 
